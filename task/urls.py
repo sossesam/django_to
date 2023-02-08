8331e74c0ef7f0781f2f39a4128b64ec033902ef
oyeniran samuel sanmi <sossesam@gmail.com>
2023-02-08 18:52:22 +0100

    all

--- task/urls.py
+++ task/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-   path('', views.home)
+   path('', views.home, name='list'),
+   path('update_task/<str:pk>/',views.updateTask, name='update_task'),
+   path('delete_task/<str:pk>/',views.deleteTask, name='delete_task')
 ]
--- task/views.py
+++ task/views.py
@@ -17,3 +17,26 @@ def home(request):
         return redirect("/")
     context = {'tasks':tasks, 'form':form}
     return render(request, 'task/list.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form =TaskForm(instance=task)
+
+    if request.method == "POST":
+        form =TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+
+        return redirect("/")
+
+    context = {'form': form}
+    return render(request,'task/update_task.html', context)
+
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+    if request.method == 'POST' :
+        task.delete()
+        return redirect('/')
+
+    context = {'task': task}
+    return render(request, "task/delete_task.html", context)
